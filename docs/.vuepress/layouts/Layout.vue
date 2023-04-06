ee72ff8f8f8fa32e10c745277168fbd309f9500c
Karsten Zhou <1339688323@qq.com>
2023-04-06 23:06:04 +0800

    Update Layout.vue

--- docs/.vuepress/layouts/Layout.vue
+++ docs/.vuepress/layouts/Layout.vue
@@ -5,22 +5,16 @@ import { defineAsyncComponent } from 'vue';
 const fulltext = defineAsyncComponent(() => import('./fulltext.vue'));
 
 function timeLocalize(lang, date) {
-    if (date === undefined) { return; } // 如果没有date就返回空值
-    let prefix = '';
-    let leftBracket = ' (';
-    let rightBracket = ')';
-    if (lang === 'zh') {
-        lang = 'zh-cn';
-        prefix = '日期：';
-        leftBracket = '（';
-        rightBracket = '）';
-    } else if (lang === 'de') {
-        prefix = 'Datum: ';
-    } else if (lang === 'en') {
-        prefix = 'Date: ';
-    }
-    moment.locale(lang);
-    return prefix + moment(date, 'YYYY-MM-DD').fromNow() + leftBracket + moment(date, 'YYYY-MM-DD').format('LL') + rightBracket;
+    if (!date) return; // 如果没有date就返回空值
+    // 使用对象字面量来存储不同语言的前缀
+    const prefix = { 'zh': '日期：', 'de': 'Datum: ', 'en': 'Date: ' }[lang] || '';
+    // 根据语言设置括号
+    const leftBracket = lang === 'zh' ? '（' : ' (';
+    const rightBracket = lang === 'zh' ? '）' : ')';
+    // 设置moment.js的语言
+    moment.locale(lang === 'zh' ? 'zh-cn' : lang);
+    // 使用模板字符串来简化字符串拼接
+    return `${prefix}${moment(date, 'YYYY-MM-DD').fromNow()}${leftBracket}${moment(date, 'YYYY-MM-DD').format('LL')}${rightBracket}`;
 }
 </script>
 
